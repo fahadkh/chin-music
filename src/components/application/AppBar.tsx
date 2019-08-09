3fe666272e9aaa804510b8c3de2d18677ed5be09
Fahad Khan <fahad.khan@rallyhealth.com>
2019-08-08 23:46:03 -0500

    Add evergreen dependencies, template appbar

---
+++ src/components/application/AppBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppBar: React.FC<AppBarProps> = (props: AppBarProps) => {
+    return (
+        <div>
+            ChinMusicReviews
+        </div>
+    )
+}
+
+export interface AppBarProps { }
+
+export default AppBar
